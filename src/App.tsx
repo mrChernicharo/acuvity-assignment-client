4755ed1866b39c38186150a0c35f7658fa639ab4
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-10-20 14:06:54 -0300

    drawing something...

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import { chart } from "./Chart";
 
 function App() {
   const [nodeId, setNodeId] = useState(1);
+  // const [chartSvg, setChartSvg] = useState<SVGSVGElement | null>(null);
 
-  const { isPending, data: nodesData } = useQuery({
+  const { isPending, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () => fetch(`http://localhost:3333/node/${nodeId}`).then((res) => res.json()),
   });
+
+  useEffect(() => {
+    if (data?.payload) {
+      const { node, links, relatedNodes } = data.payload;
+      const nodes = [node, ...relatedNodes];
+
+      const chartData = { nodes, links };
+      console.log({ chartData });
+
+      const chartSVG = chart({ nodes, links });
+
+      if (chartSVG) {
+        const canvas = document.querySelector("#canvas");
+        if (canvas) {
+          canvas.innerHTML = chartSVG;
+        }
+      }
+      // setChartSvg(res);
+    }
+  }, [data]);
 
   return (
     <div className="w-screen">
       <h1 className="text-emerald-500 text-center">Acuvity</h1>
 
-      <pre>{isPending ? "Loading..." : JSON.stringify(nodesData.payload, null, 2)}</pre>
+      {/* <pre>{isPending ? "Loading..." : JSON.stringify(data.payload, null, 2)}</pre> */}
+
+      {/* <>{chartSvg}</> */}
+      <svg id="canvas" />
     </div>
   );
 }
