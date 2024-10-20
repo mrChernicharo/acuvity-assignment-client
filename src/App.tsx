8f961f24b7ca4dddef6b44dcff40719e2791be56
Felipe Chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2024-10-20 13:10:30 -0300

    bring in tanstack Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 
 function App() {
+  const [nodeId, setNodeId] = useState(1);
+
+  const { isPending, data: nodesData } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () => fetch(`http://localhost:3333/node/${nodeId}`).then((res) => res.json()),
+  });
+
   return (
-    <div>
-      <h1 className="text-red-500">Acuvity</h1>
+    <div className="w-screen">
+      <h1 className="text-emerald-500 text-center">Acuvity</h1>
+
+      <pre>{isPending ? "Loading..." : JSON.stringify(nodesData.payload, null, 2)}</pre>
     </div>
   );
 }
